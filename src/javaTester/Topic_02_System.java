9aa4dfe00bbdc4a8d8875df4d4793f4a252898d2
trannhi <trannhi@localhost>
2022-08-12 15:57:19 +0700

    System test

---
+++ src/javaTester/Topic_02_System.java
@@ -0,0 +1,15 @@
+package javaTester;
+
+public class Topic_02_System {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);  
+		
+		String osName = System.getProperty("os.name");
+		System.out.println(osName);  
+
+	}
+
+}
